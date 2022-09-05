10a15fc5e10a77b03b582822377aa91235081c8e
elmadbouh <elmadbouh@yahoo.com>
2022-09-05 12:03:55 +0200

    update and delete function in main2.py

--- main2.py
+++ main2.py
@@ -1,3 +1,5 @@
+from ast import Delete
+from turtle import clear
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import pandas as pd
 
@@ -20,24 +22,38 @@ meta.create_all(engine)
 
 def add_person(name, age, city):
 
-	ins = person.insert().values(name = name, age=age, city=city)
+	stmt = person.insert().values(name=name, age=age, city=city)
 	conn = engine.connect()
-	result = conn.execute(ins)
+	result = conn.execute(stmt)
 
 	print(name +" has been added to the database")
+
+def delete_person(id):
+
+	stmt = person.delete().where(person.c.id == id)
+	conn = engine.connect()
+	conn.execute(stmt)
+	
+	print(f"Person with id = {id} has been deleted from the database")
+	
+def update_person(id, name, age, city):
+
+	stmt = person.update().where(person.c.id == id).values(name=name, age=age, city=city)
+	conn = engine.connect()
+	conn.execute(stmt)
+
+	print(f"Person with id = {id} has been updated")
+
 
 def get_all():
 	df = pd.read_sql_table('person', engine)
 	
 	print(df.head())
 
-mode = input("Please select mode 1-4:\n 1-Select\n 2-Add\n 3-Change\n 4-Delete\n  : ")
+mode = input("Please select mode 1-3 or press enter to display all\n 1-Add\n 2-Delete\n 3-Change\n : ")
 
 match mode:
-	case 1:
-		get_all()
-
-	case 2: 
+	case '1': 
 
 		name = input("Please enter a name: ")
 		age = input("Please enter an age: ")
@@ -45,4 +61,22 @@ match mode:
 
 		add_person(name, age, city)
 
+	case '2':
+
+		id = input("Please enter the id of the person you want to delete: ")
+
+		delete_person(id)
+
+	case '3':
+
+		id = input("Please enter the id of the person you want to update: ")
+		name = input("Please enter the new value for name: ")
+		age = input("Please enter the new value for age: ")
+		city = input("Please enter the new value for city: ")
+
+		update_person(id, name, age, city)
+
+	case _:
+
+		get_all()
 	
